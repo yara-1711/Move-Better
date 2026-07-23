fbd674f7668389420afb42776b1cd02f77b706dc
yara-1711 <>
2026-07-23 17:02:21 +0530

    bug fixed!!!!!!!!!!!!

--- nav-bar.js
+++ nav-bar.js
@@ -1,25 +1,16 @@
 class MBNavBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<div class="navbar bg-base-100 shadow-sm">
-    <div class="navbar-start">
-      <div class="dropdown">
-        <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" />
-          </svg>
+        <div class="navbar-start">
+          &nbsp;
         </div>
-        <ul tabindex="-1" class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-          <li><a>Homepage</a></li>
-          <li><a>Portfolio</a></li>
-          <li><a>About</a></li>
-        </ul>
-      </div>
-    </div>
-     <div class="navbar-center">
-      <a class="btn btn-ghost text-xl">Physiotheraphy Services</a>
-    </div>
-    
+  <div class="navbar-center">
+    <a class="btn btn-ghost text-xl">Physiotheraphy Services</a>
   </div>
+        <div class="navbar-end">
+          &nbsp;
+        </div>
+</div>
   `;
    }
 }
